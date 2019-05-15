9122930a0006c181ec1c9d6f966f2b701362caef
azzzikz <polskoy.st@gmail.com>
2019-05-15 19:36:07 +0300

    Azzzik's Branch

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Comit from Azzzik");
-        6358fd336738e9dbb6e28142a13a11346cbd94e0
+       String random = "6358fd336738e9dbb6e28142a13a11346cbd94e0"
+
 
     }
 }
