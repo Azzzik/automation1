a7194b6c44b508f27f22dc442bed895523c30dbe
nkfortest <nazariy.kushnir@gmail.com>
2019-05-15 19:47:42 +0300

    fxd

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         System.out.println("Comit from Azzzik");
 
-       String random = "6358fd336738e9dbb6e28142a13a11346cbd94e0"
+       String random = "6358fd336738e9dbb6e28142a13a11346cbd94e0";
 
         System.out.println("NK changes test");
 
